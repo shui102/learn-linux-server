b0df9c576fea4244544872e809237a56bffbe321
shui102 <1342073533@qq.com>
2025-05-12 16:19:00 +0800

    threadpool

---
+++ tcpip/threadpool/threadpool.h
@@ -0,0 +1,43 @@
+#ifndef _THREADPOOL_H
+#define _THREADPOOL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <ctype.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define  NUMBER 128
+
+typedef struct ThreadPool ThreadPool;
+// 创建线程池并初始化
+ThreadPool *threadPoolCreate(int min, int max, int queueSize);
+
+// 销毁线程池
+int threadPoolDestroy(ThreadPool* pool);
+
+// 给线程池添加任务
+void threadPoolAdd(ThreadPool* pool, void(*func)(void*), void* arg);
+
+// 获取线程池中工作的线程的个数
+int threadPoolBusyNum(ThreadPool* pool);
+
+// 获取线程池中活着的线程的个数
+int threadPoolAliveNum(ThreadPool* pool);
+
+//////////////////////
+// 工作的线程(消费者线程)任务函数
+void* worker(void* arg);
+// 管理者线程任务函数
+void* manager(void* arg);
+// 单个线程退出
+void threadExit(ThreadPool* pool);
+
+#endif
